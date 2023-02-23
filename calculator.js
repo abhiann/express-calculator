26224150002243bd1c0aa94727378925cd549554
Abhishek Ranjan <abhishekrjnn@gmail.com>
2023-02-24 02:23:32 +0530

    parse float

--- calculator.js
+++ calculator.js
@@ -1,5 +1,4 @@
 //jshint esversion:6
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -12,29 +11,21 @@ app.get("/", function(req, res){
 
 app.get("/bmicalculator", function(req, res){
     res.sendFile(__dirname +"/bmiCalculator.html");
-})
-
-
+});
 
 app.post("/", function(req,res){
     var num1 = Number(req.body.num1);
     var num2 = Number(req.body.num2);
-
     var result = num1 + num2;
-
     res.send("The result of the the calculation is " + result);
-})
+});
 
 app.post("/bmicalculator", function(req, res){
-    const weight = Number(req.body.weight);
-    const height = Number(req.body.height);
+    const weight = parseFloat(req.body.weight);
+    const height = parseFloat(req.body.height);
     const bmi = Math.round(weight/(height*height));
     res.send("Your BMI is: "+bmi);
-})
-
-
-
-
+});
 
 app.listen(3000, function(){
     console.log("App listening to server 3000");
